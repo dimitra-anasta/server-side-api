3f3ce813189d356a680bbd12e0334cb0d33593f4
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-08-30 00:12:37 -0400

    api

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
 var apiKey = "bb7882d0e26c49b9d2b261aa31868471";
 var searchForm = document.querySelector("#search-form");
-
+var header = document.querySelector(".header")
+var temp = document.querySelector("#tempHead")
+var windHead = document.querySelector("#windHead")
+var humidityHead = document.querySelector("#humidityHead")
+var cards = document.querySelector("#cards")
+var temp1 = document.querySelector("#temp1")
 //var searchInput = document.querySelector("#text").value;
 
 function citySearch(citySearch) {
-  console.log(citySearch)
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + '&units=imperial' + "&appid=" + apiKey;
-    // make lat + long variables to pass into second API, set them from the first API's response.\
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (currentWeather) {
+      console.log(currentWeather)
+      header.innerHTML = currentWeather.name + moment(currentWeather.dt, "X").format(" (MM/DD/YYYY) ") + `              <img src=" http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png" alt="" />
+      `; 
+      tempHead.innerHTML = " Temp: " + currentWeather.main.temp + "℉"
+      windHead.innerHTML = " Wind: " + currentWeather.wind.speed + " mph"
+      humidityHead.innerHTML = " Humidity: " + currentWeather.main.humidity + "%"
+    })
+  //console.log(citySearch)
 
-console.log(requestUrl)
-    fetch(requestUrl)
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + '&units=imperial' + "&appid=" + apiKey;
 
-    .then(function(response){
-        console.log(response)
-        return response.json();
-    } )
-        .then (function(data) {
-        
-        displayCards(data);
+  fetch(requestUrl)
 
-})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      displayCards(data);
 
-function displayCards(data){
-  console.log(data)
+    })
+
+  function displayCards(data) {
+    cards.innerHTML=""
+    for (i = 0; i < data.list.length; i=i+8){
+      cards.innerHTML= cards.innerHTML + `  <div class="col-sm-2">
+      <div class="card" id="card1">
+        <div class="card-body">
+          <h5 class="card-title">${moment(data.list[i].dt, "X" ).format(" (MM/DD/YYYY) ")}</h5>
+          <img src=" http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="" />;
+          <p class="card-text" id="temp1">Temp: 73°</p>
+          <p id="wind1">Wind: 8 MPH</p>
+          <p id="humidity1">Humidity: 20%</p>
+        </div>
+      </div>
+    </div>`
+
+    }
+    console.log(cards.innerHTML)
+
+  }
+  // // function getAPI() {
+  // //     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+  // //   .then(function (response) {
+  // //     return response.json();
+  // //   })
+  // //   .then(function (data) {
+  // //     console.log(data);
+  // //   });
+
 
 }
-// // function getAPI() {
-// //     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-// //   .then(function (response) {
-// //     return response.json();
-// //   })
-// //   .then(function (data) {
-// //     console.log(data);
-// //   });
 
-
- }
-        
 
 
 console.log(searchForm);
-searchForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    //capture input value using console.log below, store it in a variable => pass it in to two functions, one to capture current data the second to capture future
-    console.log(event.target.children[0].children[0].value)
-    
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //capture input value using console.log below, store it in a variable => pass it in to two functions, one to capture current data the second to capture future
+  console.log(event.target.children[0].children[0].value)
 
-     citySearch(event.target.children[0].children[0].value)
+
+  citySearch(event.target.children[0].children[0].value)
 });
 
 
